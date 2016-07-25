c1e5117d0ab8a8cf22c53cafd3e07c757bb63ed5
AlexandrPro <Alexandr.Prokofiev.A@gmail.com>
2016-07-25 18:23:02 +0300

    Add Contract layer

---
+++ WebsitePerformance/WebsitePerformance.Contracts/DAL/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsitePerformance.Contracts.DAL
+{
+    interface IRepository<T> where T: class 
+    {
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        T FindById(int id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> expression);
+        void SaveChanges();
+    }
+}
