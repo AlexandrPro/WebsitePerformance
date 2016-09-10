0c56e7e122b46618c10cecb0f45e67d82da5db03
AlexandrPro <Alexandr.Prokofiev.A@gmail.com>
2016-09-10 14:57:03 +0300

    Add all interfaces

---
+++ WebsitePerformance/WebsitePerformance.Contracts/DTO/LinkViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsitePerformance.Contracts.DTO
+{
+    class LinkViewModel
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int LastButOneTest { get; set; } //предпоследний
+        public int LastTest { get; set; } //последний
+        public int NewTest { get; set; }
+    }
+}
---
+++ WebsitePerformance/WebsitePerformance.Contracts/Services/IWebsiteService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using WebsitePerformance.Contracts.DTO;
+
+namespace WebsitePerformance.Contracts.Services
+{
+    interface IWebsiteService : IAddable<string>
+    {
+        IQueryable<LinkViewModel> TestWebsitePerformance(string url);
+    }
+}
